f8b9a32c8c9dab869a3bb9771d82e5bc96f39fae
Kamron <bahtierhasanzoda4@gmail.com>
2023-06-20 16:04:14 +0500

    Bank

---
+++ main.js
@@ -0,0 +1,28 @@
+import { reloadHeader } from "./modules/header";
+import {getData} from "./modules/http";
+import { reload, reloadTable } from "./modules/ui";
+
+let wrapper = document.querySelector('.card-container__wrapper');
+let body = document.querySelector('tbody');
+let localedUser = JSON.parse(localStorage.getItem('user')) || {}
+
+reloadHeader()
+
+//вызовы
+getData('/cards?user_id=' + localedUser?.id)
+   .then(res => {
+      if (res.status == 200 || res.status === 201) {
+         if(res.data.length > 0) {
+            reload(res.data.slice(0, 4), wrapper);
+         }
+      }
+   })
+
+getData('/transaction?user_id=' + localedUser?.id)
+   .then(res => {
+      if (res.status == 200 || res.status === 201) {
+         if(res.data.length > 0) {
+            reloadTable(res.data.slice(0, 5), body);
+         }
+      }
+   })
---
+++ modules/ui.js
@@ -0,0 +1,66 @@
+function getRandomColors() {
+    function getRGB() {
+        function randomizer() {
+            return Math.floor(Math.random() * 255)        
+        }
+    
+        let r = randomizer()
+        let g = randomizer()
+        let b = randomizer()
+
+        return `${r},${g},${b}`
+    }
+    return `linear-gradient(84.36deg,rgba(${getRGB()}, 1),  rgba(${getRGB()}, 1))`
+}
+
+export function reload(arr, place) {
+    let doc = document
+    place.innerHTML = '';
+    
+    for (let card of arr) {
+        let div = doc.createElement('div');
+        let title = doc.createElement('div');
+        let lang = doc.createElement('div');
+        //class
+        div.classList.add('item');
+        title.classList.add('item__title');
+        lang.classList.add('item__lang');
+        //inner
+        title.innerHTML = card.name;
+        lang.innerHTML = card.currency;
+        div.style.background = getRandomColors()
+        div.style.cursor = "pointer"
+        //append
+        div.append(title, lang);
+        place.append(div);
+
+        div.onclick = () => {
+            location.assign('/pages/cards/card/?id=' + card.id)
+        }
+    }
+}
+export function reloadTable(res, body) {
+    let doc = document;
+    body.innerHTML = ''
+    let n = 1;
+    for (let data of res) {
+        let tr = doc.createElement('tr');
+        for (let i = 0; i < 5; i++) {
+            let td = doc.createElement('td');
+            if (i === 0) {
+                td.innerText = n;
+            } else if (i === 1) {
+                td.innerText = data.fromWallet;
+            } else if (i === 2) {
+                td.innerText = data.categories;
+            } else if (i === 3) {
+                td.innerText = data.price;
+            } else if (i === 4) {
+                td.innerText = data.time;
+            }
+            tr.append(td);
+        }
+        body.append(tr);
+        n++;
+    }
+}
